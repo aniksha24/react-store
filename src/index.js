40c6b7fc8f95aac9a9ef947b7a65b6fb17abcb30
aniksha24 <anikshabolla@gmail.com>
2025-03-19 11:03:55 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App7 from './App7';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App7 name="John" age={65}/>);
+root.render(<App name="John" age={65}/>);
