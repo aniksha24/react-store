91ff9092dacd0e32b153f718ee838c1a09168958
aniksha24 <anikshabolla@gmail.com>
2025-03-19 09:41:56 +0530

    edited

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App3 from './App3';
+import App7 from './App7';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App3 name="John" age={65}/>);
+root.render(<App7 name="John" age={65}/>);
